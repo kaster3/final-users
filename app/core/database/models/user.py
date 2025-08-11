87e25d704ea9eccaebe9f13ba60c270be6c52556
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-11 12:53:00 +0300

    create user sqlalchemy model

---
+++ app/core/database/models/user.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
+
+from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.database.models.base import Base
+from app.core.database.models.mixins import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .company import Company
+
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    # Кастомные поля
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    phone: Mapped[str] = mapped_column(String(20), unique=True, nullable=True)
+    # Роль внутри компании, есть еще роль от fastapi-users, она для админки
+    role: Mapped[str] = mapped_column(
+        String(20),
+        default="member",  # member, team_admin, superuser
+        nullable=False
+    )
+    company_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("companies.id"),
+        nullable=True  # Пользователь может быть без компании
+    )
+    # Связи
+    company: Mapped["Company"] = relationship(back_populates="users")
+
+    # Table args
+    __table_args__ = (
+        UniqueConstraint("phone", name="uq_user_phone"),
+    )
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
