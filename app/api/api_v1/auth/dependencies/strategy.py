a4de022258cab948acb2f9074de34b3c5d0651a8
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-11 12:53:35 +0300

    create strategy to transport jwt

---
+++ app/api/api_v1/auth/dependencies/strategy.py
@@ -0,0 +1,12 @@
+from fastapi_users.authentication import JWTStrategy
+
+from app.core.settings import settings
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(
+        secret=settings.jwt_token.private_key.read_text(),
+        lifetime_seconds=settings.jwt_token.lifetime_seconds,
+        algorithm="RS256",
+        public_key=settings.jwt_token.public_key.read_text(),
+    )
