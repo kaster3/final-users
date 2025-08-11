0bd48ca04ee629693678bdf5f8190ab809849ab9
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-11 12:48:08 +0300

    add new providers to init

--- app/ioc/init_container.py
+++ app/ioc/init_container.py
@@ -1,10 +1,14 @@
 from dishka import AsyncContainer, make_async_container
 
 from app.core import Settings
+from app.ioc.service_provider import ServiceProvider
+from app.ioc.sqlalchemy_providers import SQLAlchemyProvider
 
 
 def init_async_container(settings: Settings) -> AsyncContainer:
     container = make_async_container(
+        SQLAlchemyProvider(),
+        ServiceProvider(),
         context={
             Settings: settings,
         }
