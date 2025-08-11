b1055dd834411136205e94e65b7e464c278dc5f8
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-11 12:54:53 +0300

    fastapi-users backend

---
+++ app/api/api_v1/auth/dependencies/backend.py
@@ -0,0 +1,11 @@
+from fastapi_users.authentication import AuthenticationBackend
+
+from app.core.authentication.transport import bearer_transport
+from .strategy import get_jwt_strategy
+
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
