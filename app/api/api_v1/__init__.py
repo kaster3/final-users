7fb0d79ec9e3a761a740cbd011114fcb8cf66e7b
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-12 23:58:45 +0300

    add company router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -5,12 +5,13 @@ from app.core import settings
 from .some_endpoint import router as endpoint
 from .auth.auth import router as auth
 from .auth.users import router as users
+from .company.handler import router as company
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 
-for rout in (endpoint, auth, users):
+for rout in (endpoint, auth, users, company):
     router.include_router(
         router=rout,
     )
