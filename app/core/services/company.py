aad323908ace5f487e9c06f62a9b52aac6ed8ec5
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-12 23:58:05 +0300

    create init method add create company method

--- app/core/services/company.py
+++ app/core/services/company.py
@@ -1,2 +1,19 @@
+from app.api.api_v1.company.dto import CompanyCreate
+from app.core.database.models import Company
+from app.core.interfaces.company import IDBCompanyRepository
+
+
 class CompanyService:
-    pass
+    def __init__(
+            self,
+            compony_repository: IDBCompanyRepository,
+    ) -> None:
+        self.company_repository = compony_repository
+
+    async def create_company(
+            self,
+            company_data: CompanyCreate,
+    ) -> Company:
+        # todo проврку на админа
+        company = await self.company_repository.create_company(company_data=company_data)
+        return company
