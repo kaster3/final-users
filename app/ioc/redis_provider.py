4e79b21a81d73c2c6d43ef2718726ff9d5fa7ea3
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-12 23:29:03 +0300

    add redis provider

--- app/ioc/redis_provider.py
+++ app/ioc/redis_provider.py
@@ -1,6 +1,6 @@
 from typing import AsyncIterator
 
-from redis import asyncio
+from redis import asyncio as aredis
 from dishka import Provider, Scope, provide
 
 from app.core import Settings
@@ -10,8 +10,8 @@ class RedisProvider(Provider):
     scope = Scope.APP
 
     @provide
-    async def get_redis(self, settings: Settings) -> AsyncIterator[asyncio.Redis]:
-        redis = asyncio.from_url(str(settings.redis.url))
+    async def get_redis(self, settings: Settings) -> AsyncIterator[aredis.Redis]:
+        redis = aredis.from_url(str(settings.redis.url))
         try:
             yield redis
         finally:
